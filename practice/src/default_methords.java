5427d107b218090138562330f94a187ed06bcae0
ShivaDoulagar <149285341+ShivaDoulagar@users.noreply.github.com>
2024-01-18 07:07:43 +0530

    Initial commit

---
+++ practice/src/default_methords.java
@@ -0,0 +1,33 @@
+public class default_methords {
+    public static void main(String[] args) {
+        school shiva = new school();
+        shiva.classB();
+        shiva.classC();
+    }
+}
+
+interface kindergarden{
+    void classA();
+    void classB();
+    default void classC(){
+        System.out.println("I am from class C");
+    }
+}
+
+class lower_School{
+    void class1(){
+        System.out.println("I am student form 1st class");
+    }
+    void class2(){
+        System.out.println("I am Student form class 2 ");
+    }
+}
+
+class school extends lower_School implements kindergarden{
+    public void classA(){
+        System.out.println("I am from class A");
+    }
+    public void classB(){
+        System.out.println("I am form class B");
+    }
+}
