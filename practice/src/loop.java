5629411bcb9bce1e7f354664f06425bfa5bb83c7
ShivaDoulagar <149285341+ShivaDoulagar@users.noreply.github.com>
2024-03-18 16:25:43 +0530

    Initial commit

--- practice/src/loop.java
+++ practice/src/loop.java
@@ -1,7 +1,17 @@
-public class loop {
+public class loop{
     public static void main(String[] args) {
-        for(int i=0;i<10;i++){
-            System.out.println("Hello world");
+        String name = "hello world";
+        char[] na=name.toCharArray();
+        String result="";
+        for(int i=0;i<na.length;i++){
+            for(char a='a';a<='z';a++){
+                if(na[i]==a){
+                    result+=a;
+                    System.out.println(result);
+                }
+                
+            }
         }
+        
     }
 }
